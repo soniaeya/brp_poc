f1781a501955f557102407447dac70547a413d45
Zabiullah-sz <zabiullah.shair-zaie@polymtl.ca>
2023-01-31 23:19:57 -0500

    back end done

--- server/scenario/views.py
+++ server/scenario/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ScenarioSerializer
 from .models import Scenario
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
 
 class ScenarioView(viewsets.ModelViewSet):
     serializer_class = ScenarioSerializer
     queryset = Scenario.objects.all()
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
--- server/web_project/urls.py
+++ server/web_project/urls.py
@@ -17,11 +17,21 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from scenario import views
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 
 router = routers.DefaultRouter()
-router.register(r'scenario', views.ScenarioView, 'scenario')
+router.register(r'scenarios', views.ScenarioView, 'scenario')
+# router.register(r'token', views.MyTokenObtainPairView, 'token_obtain_pair')
+# router.register(r'token/refresh/', TokenRefreshView.as_view(),'token_refresh')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 ]
